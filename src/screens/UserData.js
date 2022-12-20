7d93e3e363d5948ed778282c640ddf47a6dd2f9d
Monmoy042 <monmoykms.16@gmail.com>
2022-12-20 21:19:01 +0600

    user data finished and using of external api done

--- src/screens/UserData.js
+++ src/screens/UserData.js
@@ -1,14 +1,134 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, Text, FlatList, Image } from "react-native";
 
 const UserData = () => {
+  const [isLoaded, setIsLoaded] = useState(true);
+  const [myData, setMyData] = useState([]);
+
+  const getUserData = async () => {
+    try {
+      const response = await fetch(
+        "https://thapatechnical.github.io/userapi/users.json"
+      );
+      const realData = await response.json();
+      setMyData(realData);
+      setIsLoaded(false);
+      // console.log(myData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // Fetch Data From External API
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  // Show Students Cards
+  const showUserData = ({ item }) => {
+    return (
+      <View style={styles.card}>
+        <View style={styles.imgContainer}>
+          <Image style={styles.imgStyle} source={{ uri: item.image }} />
+        </View>
+
+        <View>
+          <View style={styles.bioDataContainer}>
+            <Text style={styles.bioData}>Bio-Data </Text>
+            <Text style={styles.idNumber}>
+              {item.id < 10 ? `#0${item.id}` : `#{item.id}`}
+            </Text>
+          </View>
+          <View style={styles.mainContent}>
+            <Text style={styles.myName}>Name:{item.name}</Text>
+            <Text style={styles.myName}>Email:{item.email}</Text>
+            <Text style={styles.myName}>Mobile:{item.mobile}</Text>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <View>
-      <Text>User Data</Text>
+      <Text style={styles.mainHeader}>List of Students</Text>
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={myData}
+        renderItem={showUserData}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainContainer: {
+    width: "100%",
+    minHeight: "100%",
+    paddingVertical: 50,
+    backgroundColor: "#ebedee",
+  },
+  card: {
+    width: 250,
+    height: 350,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    margin: 20,
+  },
+  imgStyle: {
+    width: "100%",
+    height: undefined,
+    aspectRatio: 1,
+  },
+  bioDataContainer: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#353535",
+    paddingVertical: 10,
+    fontFamily: "",
+  },
+  idNumber: {
+    fontSize: 20,
+    color: "rgba(255,255,255,0.5)",
+    fontFamily: "",
+    paddingRight: 5,
+  },
+  bioData: {
+    fontSize: 30,
+    color: "#fff",
+    fontFamily: "",
+  },
+  mainHeader: {
+    fontSize: 30,
+    color: "#a18ce5",
+    textAlign: "center",
+    fontFamily: "JosefinSans_400Regular",
+  },
+  imgContainer: {
+    padding: 10,
+  },
+  imgStyle: {
+    width: "100%",
+    height: 180,
+  },
+  mainContent: {
+    padding: 10,
+    backgroundColor: "#353535",
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+  myName: {
+    fontSize: 14,
+    color: "#fff",
+    marginBottom: 10,
+    alignSelf: "flex-start",
+    textTransform: "capitalize",
+    fontFamily: "JosefinSans_400Regular",
+  },
+});
 
 export default UserData;
